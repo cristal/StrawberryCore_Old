3f895ea051825ca15695c447b534625aac12e9b2
Kitiara <vinas_solamnus1@hotmail.com>
2011-10-07 00:56:48 +0300

    Update for GetRealGOEntry function
    Signed-off-by: Kitiara <vinas_solamnus1@hotmail.com>

--- src/server/game/Server/WorldSession.h
+++ src/server/game/Server/WorldSession.h
@@ -413,7 +413,7 @@ class WorldSession
         bool IsARecruiter() { return isRecruiter; }
         uint64 GetRealGUID(uint8 packetGuid, uint8 byte, std::string ErrorMessage);
         uint64 GetRealGOGUID() { return realgoguid; }
-        uint64 GetRealGOEntry() { return realgoentry; }
+        uint32 GetRealGOEntry() { return realgoentry; }
 
     public:                                                 // opcodes handlers
 
