530b40ce77768059c4a4c174acb380d886107e7f
Fabian <fabian@strawberry-pr0jcts.com>
2011-12-29 01:42:55 +0100

    [243] CMSG_REQUEST_CHARACTER_DELETE use a simple uint64 as guid, also fixed char deletion.

--- src/server/game/Server/Protocol/Handlers/CharacterHandler.cpp
+++ src/server/game/Server/Protocol/Handlers/CharacterHandler.cpp
@@ -793,15 +793,9 @@ void WorldSession::HandleCharCreateCallback(PreparedQueryResult result, Characte
 void WorldSession::HandleCharDeleteOpcode(WorldPacket & recv_data)
 {
     uint64 guid = 0;
-    uint8 packetGuid, byte;
 
     sLog->outStaticDebug("WORLD: Received Player Delete Message");
-    recv_data >> packetGuid;
-    recv_data >> byte;
-    recv_data.read_skip<uint16>();
-    recv_data.read_skip<uint32>();
-
-    guid = GetRealCharGUID(packetGuid, byte, "Can't delete anything, sorry!");
+    recv_data >> guid;
     if (guid == -1)
         return;
 
